b14ce0e94bba0354bf78ada12ab1c39307a86592
Nishant Parwani <nishantparwani2007@gmail.com>
2026-09-23 11:19:47 +0530

    N075: Configure Firebase App Check

--- app/src/main/java/com/example/n075nishantassgn1/MainActivity.kt
+++ app/src/main/java/com/example/n075nishantassgn1/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.n075nishantassgn1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
+import androidx.composetime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
